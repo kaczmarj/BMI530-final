264bd255b76321840570707c8c21dd6e61585c4f
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2021-11-15 15:37:42 -0500

    add 3d plot and slider for num samples

--- app.R
+++ app.R
@@ -7,6 +7,10 @@
 #    http://shiny.rstudio.com/
 #
 
+# How to show interactive 3d graphics using webgl:
+# https://stackoverflow.com/q/44100268/5666087
+
+library(rgl)
 library(shiny)
 
 # Define UI ----
@@ -21,22 +25,30 @@ ui <- fluidPage(
         "geneQuery",
         "Select a gene",
         c("KRAS", "TP53"),
-      )
+      ),
+      sliderInput("numSamples", label = "n samples", min = 10, max = 100, value = 10, step = 10),
     ),
 
     # Space for plots to be rendered ----
     mainPanel(
-      textOutput("textOfGeneQuery")
+      textOutput("textOfGeneQuery"),
+      rglwidgetOutput("plot3d", width = 800, height = 600),
     ),
   )
 )
 
 # Define server logic required ----
 server <- function(input, output) {
-
   # Render text based on the user input ----
   output$textOfGeneQuery <- renderText({
     sprintf("You selected %s", input$geneQuery)
+  })
+
+  output$plot3d <- renderRglwidget({
+    n <- input$numSamples
+    try(close3d())
+    plot3d(rnorm(n), rnorm(n), rnorm(n))
+    rglwidget()
   })
 }
 
